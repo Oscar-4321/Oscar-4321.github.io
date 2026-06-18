b5366a9af1083c96e89e20f0005cecedcfefdc1b
Oscar-4321 <135011882+Oscar-4321@users.noreply.github.com>
2026-06-18 22:02:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Website loaded!");
+function likeFilm(button) {
+  if (button.innerText === "❤️ Like") {
+    button.innerText = "💖 Liked";
+    button.style.background = "green";
+  } else {
+    button.innerText = "❤️ Like";
+    button.style.background = "#ff4d4d";
+  }
+}
