21bfa1117a2660642e7b8db75d2f1047717bfd92
Oscar-4321 <135011882+Oscar-4321@users.noreply.github.com>
2026-06-18 20:58:50 +0100

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,49 @@
+body {
+  margin: 0;
+  padding: 40px 20px;
+  background: #ffffff;
+  color: #000;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, sans-serif;
+  line-height: 1.6;
+}
+
+main {
+  max-width: 720px;
+  margin: 0 auto;
+}
+
+nav {
+  margin-bottom: 40px;
+}
+
+nav a {
+  color: #0000ee;
+  text-decoration: none;
+  font-weight: 500;
+}
+
+nav a:hover {
+  text-decoration: underline;
+}
+
+h1, h2, h3 {
+  font-weight: 600;
+}
+
+a {
+  color: #0000ee;
+}
+
+a:visited {
+  color: #551a8b;
+}
+
+ul {
+  padding-left: 20px;
+}
+
+hr {
+  border: none;
+  border-top: 1px solid #000;
+  margin: 40px 0;
+}
